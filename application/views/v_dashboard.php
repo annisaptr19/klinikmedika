215d31bd8cb52612c5d65c210f7a29b24055cea2
annisaptr19 <annisaputri0478@gmail.com>
2021-12-21 19:34:22 +0700

    first commite

---
+++ application/views/v_dashboard.php
@@ -0,0 +1,5 @@
+<div class="container-fluid mt-4">
+    <div class="jumbotron">
+        <h1 class="display-4">Klinik Medika</h1>
+        <p class="lead">Klinik Terbaik Pilihan Keluarga Anda</p>
+    </div>
